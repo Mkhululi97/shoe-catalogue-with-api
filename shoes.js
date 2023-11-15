85685a60ea019296a0ff2c534bda3835169a2ff6
mkhululi <mkhululimtshali@gmail.com>
2023-11-15 10:20:06 +0200

    added sign in and sign up pages

--- shoes.js
+++ shoes.js
@@ -4,29 +4,60 @@ document.addEventListener("alpine:init", () => {
     return {
       title: "Shoe-catalogue",
       shoes: [],
+      signUpErrors: "",
       email: "test@test.com",
+      password: "",
       cartShoes: [],
       cartTotal: 0.0,
-      open: false,
       showCart: false,
-
-      hideModal() {
-        return (this.open = false);
-      },
+      userExists: true,
+      auth: false,
       openCart() {
         return (this.showCart = true);
       },
       closeCart() {
         return (this.showCart = false);
       },
+      signUp(email, password) {
+        const response = axios.post(
+          "https://shoes-api-dkj2.onrender.com/api/users/signup",
+          {
+            email: email,
+            password: password,
+            is_admin: false,
+          }
+        );
+        response.then((res) => {
+          this.signUpErrors = res.data.message;
+          if (res.data.message !== "User Created") {
+            //turn userExists to true
+            this.userExists = false;
+            //x:bind href to login.html
+            //show login form
+          }
 
+          setTimeout(() => {
+            this.signUpErrors = "";
+          }, 2500);
+        });
+        return this.userExists;
+      },
+      signIn(email, password) {
+        // "https://shoes-api-dkj2.onrender.com/api/users/login",
+        const response = axios.post("http://localhost:3004/api/users/login", {
+          email: email,
+          password: password,
+        });
+        console.log(email, password);
+        response.then((res) => console.log(res));
+      },
       getCart() {
         const getCartUrl = `https://shoes-api-dkj2.onrender.com/api/cart/${this.email}`;
         return axios.get(getCartUrl);
       },
       addShoeOnCart(shoeid) {
         const addShoeUrl = "https://shoes-api-dkj2.onrender.com/api/cart/add";
-        this.open = true;
+
         return axios.post(addShoeUrl, {
           email: this.email,
           shoe_id: shoeid,
@@ -73,6 +104,13 @@ document.addEventListener("alpine:init", () => {
           shoe_id: shoeid,
         });
         this.showCartData();
+      },
+      checkuser() {
+        if (userExists) {
+          alert("returns");
+        } else {
+          alert("new");
+        }
       },
     };
   });
