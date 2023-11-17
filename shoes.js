be675d27f84174a68f7af4b5d68e5e747f29fcd9
mkhululi <mkhululimtshali@gmail.com>
2023-11-17 22:10:12 +0200

    added filters

--- shoes.js
+++ shoes.js
@@ -12,6 +12,92 @@ document.addEventListener("alpine:init", () => {
       showCart: false,
       userExists: true,
       auth: false,
+      brand: "",
+      size: 0,
+      color: "",
+      warning: 3,
+      handleChange() {
+        //filters by brand size and color
+        if (this.brand !== "" && this.size !== "" && this.color !== "") {
+          this.shoes = [];
+          axios
+            .get(
+              `https://shoes-api-dkj2.onrender.com/api/shoes/brand/${this.brand}/size/${this.size}/color/${this.color}`
+            )
+            .then((result) => {
+              this.shoes = result.data;
+            });
+        } //filters by size
+        else if (this.size !== "" && this.brand == "" && this.color == "") {
+          this.shoes = [];
+          axios
+            .get(
+              `https://shoes-api-dkj2.onrender.com/api/shoes/size/${this.size}`
+            )
+            .then((result) => {
+              this.shoes = result.data;
+            });
+        } //filters by brand
+        else if (this.brand !== "" && this.size == "" && this.color == "") {
+          this.shoes = [];
+          axios
+            .get(
+              `https://shoes-api-dkj2.onrender.com/api/shoes/brand/${this.brand}`
+            )
+            .then((result) => {
+              this.shoes = result.data;
+            });
+        } //filters by color
+        else if (this.color !== "" && this.brand == "" && this.size == "") {
+          this.shoes = [];
+          axios
+            .get(
+              `https://shoes-api-dkj2.onrender.com/api/shoes/color/${this.color}`
+            )
+            .then((result) => {
+              this.shoes = result.data;
+            });
+        } //filters by brand and size
+        else if (this.brand !== "" && this.size !== "" && this.color == "") {
+          this.shoes = [];
+          axios
+            .get(
+              `https://shoes-api-dkj2.onrender.com/api/shoes/brand/${this.brand}/size/${this.size}`
+            )
+            .then((result) => {
+              this.shoes = result.data;
+            });
+        } //filters by brand and color
+        else if (this.brand !== "" && this.color !== "" && this.size == "") {
+          this.shoes = [];
+          axios
+            .get(
+              `https://shoes-api-dkj2.onrender.com/api/shoes/brand/${this.brand}/color/${this.color}`
+            )
+            .then((result) => {
+              this.shoes = result.data;
+            });
+        } //filter by size and color
+        else if (this.color !== "" && this.size !== "" && this.brand !== "") {
+          this.shoes = [];
+          axios
+            .get(
+              `https://shoes-api-dkj2.onrender.com/api/shoes/size/${this.size}/color/${this.color}`
+            )
+            .then((result) => {
+              this.shoes = result.data;
+            });
+        }
+        //no filters
+        else if (this.brand === "" && this.size === "" && this.color === "") {
+          this.shoes = [];
+          axios
+            .get("https://shoes-api-dkj2.onrender.com/api/shoes")
+            .then((result) => {
+              this.shoes = result.data;
+            });
+        }
+      },
       openCart() {
         return (this.showCart = true);
       },
@@ -87,7 +173,8 @@ document.addEventListener("alpine:init", () => {
           });
         this.showCartData();
       },
-      addShoeToCart(shoeid) {
+      addShoeToCart(shoeid, shoes) {
+        console.log(shoes);
         this.addShoeOnCart(shoeid).then(() => {
           this.showCartData();
         });
